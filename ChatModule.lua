ba995bf6e638f9ebe293af96f00b46bc2f14e8ed
The coolest farley <surelyilikemen@gmail.com>
2024-08-10 03:09:11 -0500

    Update ChatModule.lua

--- ChatModule.lua
+++ ChatModule.lua
@@ -1,157 +1,235 @@
+-- Sigh this module is so annoying too make. Gonna have to make topbar plus inside of this too.
+
+local TextChatService = game:GetService("TextChatService")
+local UserInputService = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+local Players = game:GetService("Players")
+
+local function tween(object, goal, speed, callback)
+	local tween = TweenService:Create(object, TweenInfo.new(speed or .2, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), goal)
+	tween.Completed:Connect(callback or function() end)
+	tween:Play()
+end
+
 local module = {}
 
-local MessagingService = game:GetService("MessagingService")
-local TCS = game:GetService("TextChatService")
-local Config = game:GetService("HttpService"):JSONDecode(readfile("@FarlsXavier\\UiConfig.json"))
-local UiTools = loadstring(game:HttpGet("https://raw.githubusercontent.com/Farls-Xavier/UiLibraryV2/main/UiTools.lua"))()
-local Players = game:GetService("Players")
-local UserInputService = game:GetService("UserInputService")
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 
-function OnIncomingMessage(message:TextChatMessage)
-	local Properties = Instance.new("TextChatMessageProperties")
-	Properties.Text = message.Text
+module.__index = module
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = Players.LocalPlayer.PlayerGui
+
+local BackFrame = Instance.new("Frame")
+local UICornerBackFrame = Instance.new("UICorner")
+local ChatBox = Instance.new("Frame")
+local UICornerChatBox = Instance.new("UICorner")
+local UIStrokeChatBox = Instance.new("UIStroke")
+local TextBox = Instance.new("TextBox")
+local Messages = Instance.new("ScrollingFrame")
+local UICornerMessages = Instance.new("UICorner")
+local UIListLayoutMessages = Instance.new("UIListLayout")
+local TextLabelTemplate = Instance.new("TextLabel")
+local GradientHolder = Instance.new("Frame")
+local UIGradientHolder = Instance.new("UIGradient")
+local UICornerGradientHolder = Instance.new("UICorner")
+
+BackFrame.Name = "BackFrame"
+BackFrame.Parent = ScreenGui
+BackFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+BackFrame.BackgroundTransparency = 1.000
+BackFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+BackFrame.BorderSizePixel = 0
+BackFrame.Position = UDim2.new(0.00801974069, 0, 0.0132145705, 0)
+BackFrame.Size = UDim2.new(0, 450, 0, 290)
+
+UICornerBackFrame.CornerRadius = UDim.new(0, 4)
+UICornerBackFrame.Name = "UICornerBackFrame"
+UICornerBackFrame.Parent = BackFrame
+
+ChatBox.Name = "ChatBox"
+ChatBox.Parent = BackFrame
+ChatBox.BackgroundColor3 = Color3.fromRGB(11, 11, 11)
+ChatBox.BackgroundTransparency = 0.970
+ChatBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ChatBox.BorderSizePixel = 0
+ChatBox.Position = UDim2.new(0.0155555559, 0, 0.875862062, 0)
+ChatBox.Size = UDim2.new(0, 436, 0, 30)
+ChatBox.ZIndex = 2
+
+UICornerChatBox.CornerRadius = UDim.new(0, 4)
+UICornerChatBox.Name = "UICornerChatBox"
+UICornerChatBox.Parent = ChatBox
+
+UIStrokeChatBox.Color = Color3.fromRGB(154, 154, 154)
+UIStrokeChatBox.Thickness = 0.699999988079071
+UIStrokeChatBox.Name = "UIStrokeChatBox"
+UIStrokeChatBox.Parent = ChatBox
+
+TextBox.Parent = ChatBox
+TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.BackgroundTransparency = 1.000
+TextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.BorderSizePixel = 0
+TextBox.Position = UDim2.new(0.0137614682, 0, 0, 0)
+TextBox.Size = UDim2.new(0.986238539, 0, 1, 0)
+TextBox.Font = Enum.Font.GothamMedium
+TextBox.PlaceholderColor3 = Color3.fromRGB(249, 249, 249)
+TextBox.PlaceholderText = "Click here or use '/' to chat"
+TextBox.Text = ""
+TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.TextSize = 14.000
+TextBox.TextTransparency = 0.440
+TextBox.TextXAlignment = Enum.TextXAlignment.Left
+TextBox.ZIndex = 3
+
+Messages.Name = "Messages"
+Messages.Parent = BackFrame
+Messages.Active = true
+Messages.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Messages.BackgroundTransparency = 1.000
+Messages.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Messages.BorderSizePixel = 0
+Messages.Position = UDim2.new(3.3908421e-08, 0, 0, 0)
+Messages.Size = UDim2.new(0.99999994, 0, 0.855172396, 0)
+Messages.ScrollBarThickness = 5
+Messages.ScrollBarImageTransparency = 1
+
+UICornerMessages.CornerRadius = UDim.new(0, 4)
+UICornerMessages.Name = "UICornerMessages"
+UICornerMessages.Parent = Messages
+
+UIListLayoutMessages.Name = "UIListLayoutMessages"
+UIListLayoutMessages.Parent = Messages
+UIListLayoutMessages.SortOrder = Enum.SortOrder.LayoutOrder
+
+TextLabelTemplate.Parent = Messages
+TextLabelTemplate.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabelTemplate.BackgroundTransparency = 1.000
+TextLabelTemplate.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabelTemplate.TextTransparency = 1
+TextLabelTemplate.BorderSizePixel = 0
+TextLabelTemplate.Size = UDim2.new(0, 200, 0, 20)
+TextLabelTemplate.Font = Enum.Font.MontserratMedium
+TextLabelTemplate.RichText = true
+TextLabelTemplate.Text = "<font weight=\"bold\"><font face=\"Montserrat\"><font color=\"#EFC3CA\">[Tag]</font></font></font> <font color=\"#FFDE59\">Farleyy</font>: Text Message"
+TextLabelTemplate.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabelTemplate.TextSize = 15.000
+TextLabelTemplate.Visible = false
+TextLabelTemplate.TextXAlignment = Enum.TextXAlignment.Left
+
+GradientHolder.Name = "GradientHolder"
+GradientHolder.Parent = BackFrame
+GradientHolder.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+GradientHolder.BackgroundTransparency = 1.000
+GradientHolder.BorderColor3 = Color3.fromRGB(0, 0, 0)
+GradientHolder.BorderSizePixel = 0
+GradientHolder.Position = UDim2.new(-3.3908421e-08, 0, 0.875862062, 0)
+GradientHolder.Size = UDim2.new(0, 450, 0, 36)
+
+UIGradientHolder.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(0, 0, 0)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 0, 0))}
+UIGradientHolder.Rotation = 90
+UIGradientHolder.Transparency = NumberSequence.new{NumberSequenceKeypoint.new(0.00, 1.00), NumberSequenceKeypoint.new(1.00, 0.82)}
+UIGradientHolder.Name = "UIGradientHolder"
+UIGradientHolder.Parent = GradientHolder
+
+UICornerGradientHolder.CornerRadius = UDim.new(0, 4)
+UICornerGradientHolder.Name = "UICornerGradientHolder"
+UICornerGradientHolder.Parent = GradientHolder
+
+BackFrame.MouseEnter:Connect(function()
+	tween(GradientHolder, {Transparency = 0})
+	tween(Messages, {ScrollBarImageTransparency = 0})
+	tween(ChatBox, {Transparency = .65})
+	tween(ChatBox, {BackgroundColor3 = Color3.fromRGB(0,0,0)})
+end)
+
+BackFrame.MouseLeave:Connect(function()
+	tween(GradientHolder, {Transparency = 1})
+	tween(Messages, {ScrollBarImageTransparency = 1})
+	tween(ChatBox, {Transparency = .970})
+	tween(ChatBox, {BackgroundColor3 = Color3.fromRGB(11,11,11)})
+end)
+
+UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
+	if gameProcessedEvent then return end
 	
-	if message.TextSource then
-		local player = Players:GetPlayerByUserId(message.TextSource.UserId)
-		if player == Players.LocalPlayer then
-			Properties.PrefixText = "<font color='#FFFF00'>[You]</font> " .. message.PrefixText
-		elseif Players.LocalPlayer:IsFriendsWith(player.UserId) then
-			--Properties.PrefixText = "<font color='#C90588'>[Friend]</font> " .. message.PrefixText
-			Properties.PrefixText = "<font color='#FE0AAC'>[Friend]</font> " .. message.PrefixText
-		end
+	if input.KeyCode == Enum.KeyCode.Slash then
+		task.wait(0.01)
+		TextBox:CaptureFocus()
 	end
-	return Properties
+end)
+
+local ColorTable = {
+	"#3E8EC6",
+	"#8D6F64",
+	"#A63939"
+}
+
+function module:GetTextChatVersion()
+	return TextChatService.ChatVersion
+end
+
+function module.RenderMessage(Sender, Message)
+	local msg = TextLabelTemplate:Clone()
+	msg.Parent = Messages
+	tween(msg, {TextTransparency = 0}, .1)
+	msg.Visible = true
+	msg.Name = "MessageLabel"
+	if Sender == Players.LocalPlayer then
+		msg.Text = "<font color=\"#FFDE59\">"..Sender.DisplayName.."</font>: ".."<b>"..tostring(Message).."</b>"
+	else
+		msg.Text = "<font color=\""..ColorTable[math.random(1, #ColorTable)].."\">"..Sender.DisplayName.."</font>: ".."<b>"..tostring(Message).."</b>"
+	end
 end
 
 function module.SendMessage(Message, Recipent)
-    if Message == nil or string.len(Message) == 0 then
-        return
-    end
-    Recipent = Recipent or "All"
-
-    if TCS.ChatVersion == Enum.ChatVersion.LegacyChatService then
-        local args = {
-            [1] = Message,
-            [2] = Recipent,
-        }
-        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
-    else
-        TCS:WaitForChild("TextChannels").RBXGeneral:SendAsync(tostring(Message))
-    end
+	if module:GetTextChatVersion() == Enum.ChatVersion.LegacyChatService and string.len(TextBox.Text) > 0 then
+		local args = {
+			[1] = Message,
+			[2] = Recipent or "All",
+		}
+		game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+	elseif module:GetTextChatVersion() == Enum.ChatVersion.TextChatService then
+		TextChatService:WaitForChild("TextChannels").RBXGeneral:SendAsync(tostring(Message))
+	end
 end
 
-function module.Init()
-    --[[if not Config.CustomChat then
-        if TCS.ChatVersion == Enum.ChatVersion.LegacyChatService then
-            printColor("Game is using LegacyChatService custom tags wont work", Color3.fromRGB(255,0,0))
-            return
-        end
-        TCS.OnIncomingMessage = OnIncomingMessage
-        return
-    end]]
+function module.ConnectPlayerChatEvent(player)
+	if module:GetTextChatVersion() == Enum.ChatVersion.LegacyChatService then
+		player.Chatted:Connect(function(Message)
 
-    game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+		end)
+	elseif module:GetTextChatVersion() == Enum.ChatVersion.TextChatService then
+		TextChatService.OnIncomingMessage = function(Message:TextChatMessage)
+			local sender = Players:GetPlayerByUserId(Message.TextSource.UserId)
 
-    for i,v in pairs(game.CoreGui:GetDescendants()) do
-        if v:IsA("StringValue") and v.Name == "ReferenceValue" and v.Parent.Name == "UiLibChat" then
-            warn("DELETING OLD SCRIPT(IF NOT RIGHT THEN TELL ME)")
-            v.Parent:Destroy()
-        end
-    end
-
-    local ScreenGui = Instance.new("ScreenGui")
-    ScreenGui.Name = "UiLibChat"
-    ScreenGui.IgnoreGuiInset = true
-    ScreenGui.Parent = game.CoreGui
-
-    Instance.new("StringValue", ScreenGui).Name = "ReferenceValue"
-
-    local BackFrame = Instance.new("Frame")
-    local ChatBox = Instance.new("TextBox")
-    local MessageHolder = Instance.new("ScrollingFrame")
-    local MessageHolderListLayout = Instance.new("UIListLayout")
-    local TemplateMessage = Instance.new("TextLabel")
-
-    BackFrame.Name = "BackFrame"
-    BackFrame.Parent = ScreenGui
-    BackFrame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-    BackFrame.BackgroundTransparency = 0.500
-    BackFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    BackFrame.BorderSizePixel = 0
-    BackFrame.Position = UDim2.new(0.0339296721, 0, 0.225563914, 0)
-    BackFrame.Size = UDim2.new(0, 500, 0, 400)
-
-    ChatBox.Name = "ChatBox"
-    ChatBox.Parent = BackFrame
-    ChatBox.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-    ChatBox.BackgroundTransparency = 0.500
-    ChatBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    ChatBox.BorderSizePixel = 0
-    ChatBox.Position = UDim2.new(0, 0, 0.932500005, 0)
-    ChatBox.Size = UDim2.new(0, 500, 0, 27)
-    ChatBox.Font = Enum.Font.GothamMedium
-    ChatBox.PlaceholderColor3 = Color3.fromRGB(200, 200, 200)
-    ChatBox.PlaceholderText = "/ or press me to start chatting"
-    ChatBox.Text = ""
-    ChatBox.TextColor3 = Color3.fromRGB(200,200,200)
-    ChatBox.TextSize = 15.000
-
-    MessageHolder.Name = "MessageHolder"
-    MessageHolder.Parent = BackFrame
-    MessageHolder.Active = true
-    MessageHolder.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    MessageHolder.BackgroundTransparency = 1.000
-    MessageHolder.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    MessageHolder.BorderSizePixel = 0
-    MessageHolder.Size = UDim2.new(0, 500, 0, 373)
-    MessageHolder.ScrollBarThickness = 6
-
-    MessageHolderListLayout.Name = "MessageHolderListLayout"
-    MessageHolderListLayout.Parent = MessageHolder
-    MessageHolderListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-
-    TemplateMessage.Name = "TemplateMessage"
-    TemplateMessage.Parent = MessageHolder
-    TemplateMessage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    TemplateMessage.BackgroundTransparency = 1.000
-    TemplateMessage.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    TemplateMessage.BorderSizePixel = 0
-    TemplateMessage.Size = UDim2.new(0, 500, 0, 15)
-    TemplateMessage.Visible = false
-    TemplateMessage.Font = Enum.Font.GothamMedium
-    TemplateMessage.Text = "[Farleyy]: Test Message"
-    TemplateMessage.TextColor3 = Color3.fromRGB(255, 255, 255)
-    TemplateMessage.TextSize = 15.000
-    TemplateMessage.TextXAlignment = Enum.TextXAlignment.Left
-
-    UiTools.MakeDraggable(BackFrame, BackFrame)
-
-    local function RenderMessage(Sender:Player, Message:string)
-        local label = TemplateMessage:Clone()
-        label.Parent = MessageHolder
-        label.Text = "["..Sender.DisplayName.."]: "..tostring(Message)
-        label.Visible = true
-    end
-
-    UserInputService.InputBegan:Connect(function(input)
-        if input.KeyCode == Enum.KeyCode.Slash and not ChatBox:IsFocused() then
-            ChatBox:CaptureFocus()
-        end
-    end)
-
-    ChatBox.FocusLost:Connect(function(enterPressed)
-        if enterPressed then
-            module.SendMessage(ChatBox.Text, "All")
-            RenderMessage(Players.LocalPlayer, ChatBox.Text)
-            ChatBox.Text = ""
-        end
-    end)
-
-    for i,v in pairs(Players:GetPlayers()) do
-        v.Chatted:Connect(function(message)
-            RenderMessage(v, message)
-        end)
-    end
+		end
+	end
 end
+
+TextBox.Focused:Connect(function()
+	if ChatBox.Transparency ~= .65 then
+		tween(ChatBox, {Transparency = .65})
+	end
+	if ChatBox.BackgroundColor3 ~= Color3.fromRGB(0,0,0) then
+		tween(ChatBox, {BackgroundColor3 = Color3.fromRGB(0,0,0)})
+	end
+	if TextBox.TextTransparency ~= 0 then
+		tween(TextBox, {TextTransparency = 0})
+	end
+end)
+
+TextBox.FocusLost:Connect(function(ep)
+	tween(ChatBox, {Transparency = .970})
+	tween(ChatBox, {BackgroundColor3 = Color3.fromRGB(11,11,11)})
+	tween(TextBox, {TextTransparency = .44})
+
+	if ep then
+		module.SendMessage(TextBox.Text, "All")
+		module.RenderMessage(Players.LocalPlayer, TextBox.Text)
+	end
+	TextBox.Text = ""
+end)
 
 return module
