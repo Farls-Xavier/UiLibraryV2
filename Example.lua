680f6a9837ed10fc8418806263448be6fe4f6bb8
The coolest farley <surelyilikemen@gmail.com>
2024-10-13 18:59:57 -0500

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -5,9 +5,17 @@ local Window = Library:Window({Title = "Example", ToolTipColor = "White"}) -- Wh
 
 local Tab = Window:Tab()
 
-local Button = Tab:Button({Text = "A Button", ToolTip = "Press me"})
+local Label = Tab:Label({Text = "A Label with tooltip", ToolTip = "Read me"})
+local Button = Tab:Button({Text = "A Button", ToolTip = "Press me", Callback = function()
+    Label:TypeWrite("Test 123!")
+end})
+
+local NumberBox = Tab:NumberBox({Text = "A NumBox", Callback = function(v)
+    Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
+end})
+
 local Toggle = Tab:Toggle({Text = "A Toggle", ToolTip = "Toggle me"})
 local Slider = Tab:Slider({Text = "A Slider", ToolTip = "Slide me"})
 local Image = Tab:ImageLabel({Image = "rbxassetid://133772874267731", ToolTip = "View me"})
 local Bind = Tab:Keybind({Text = "A Bind", ToolTip = "Press my key"})
-local Label = Tab:Label({Text = "A Label with tooltip", ToolTip = "Read me"})
+local NumberBox = Tab:NumberBox({Text = "A NumberBox"})
